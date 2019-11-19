60bf338c897e233c2b450f1221a4df771decf802
Silencegod <gosha-95@bk.ru>
2019-11-20 00:50:37 +0300

    Added allure-android support

--- app/src/androidTest/java/com/moonpi/swiftnotes/test/AbstractSwiftnotesTest.kt
+++ app/src/androidTest/java/com/moonpi/swiftnotes/test/AbstractSwiftnotesTest.kt
@@ -3,6 +3,8 @@ package com.moonpi.swiftnotes.test
 import android.util.Log
 import org.junit.Before
 import com.moonpi.swiftnotes.util.targetContext
+import org.junit.BeforeClass
+import java.io.File
 
 abstract class AbstractSwiftnotesTest {
 
@@ -20,5 +22,18 @@ abstract class AbstractSwiftnotesTest {
     private fun clearAppFiles() {
         targetContext.applicationContext.filesDir.deleteRecursively()
         Log.d("Setup", "App files have been cleared")
+    }
+
+    companion object {
+        private const val ALLURE_DIR_PATH = "sdcard/allure-results"
+
+        @JvmStatic
+        @BeforeClass
+        fun clearAllureReports() {
+            File(ALLURE_DIR_PATH).listFiles().forEach {
+                it.delete()
+            }
+            Log.d("Setup", "Allure reports have been cleared")
+        }
     }
 }
--- app/src/androidTest/java/com/moonpi/swiftnotes/test/SimpleTest.kt
+++ app/src/androidTest/java/com/moonpi/swiftnotes/test/SimpleTest.kt
@@ -12,18 +12,26 @@ import org.hamcrest.CoreMatchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import ru.tinkoff.allure.android.deviceScreenshot
+import ru.tinkoff.allure.annotations.DisplayName
+import ru.tinkoff.allure.step
 
 @RunWith(AndroidJUnit4::class)
+@DisplayName("Создание заметки")
 class SimpleTest : AbstractSwiftnotesTest() {
 
     @get:Rule
     val rule = ActivityTestRule<MainActivity>(MainActivity::class.java, false, false)
 
     @Test
+    @DisplayName("Проверка открытия страницы создания")
     fun newNoteHints() {
         rule.launchActivity(null)
-        onView(withId(R.id.newNote)).perform(click())
-        onView(allOf(withId(R.id.titleEdit), isDisplayed())).check(matches(withHint("Title")))
-        onView(allOf(withId(R.id.bodyEdit), isDisplayed())).check(matches(withHint("Note")))
+        step("Проверяем отображение страницы") {
+            onView(withId(R.id.newNote)).perform(click())
+            onView(allOf(withId(R.id.titleEdit), isDisplayed())).check(matches(withHint("Title")))
+            onView(allOf(withId(R.id.bodyEdit), isDisplayed())).check(matches(withHint("Note")))
+            deviceScreenshot("page_display")
+        }
     }
 }
